aee0a4427e37eeb7c996551884d1836e1b360afc
Ruslan_Zubtsov <Ruslan_Zubtsov@epam.com>
2022-03-20 00:20:50 +0400

    added watermelon problem from codeforces; added TODOs

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # competitive-programming
 
 TODO: compose a common tex file using imports
+TODO: acm.timus.ru; hackerrank.com; leetcode.com
---
+++ codeforces.com/4A/Solution.py
@@ -0,0 +1,5 @@
+watermelon_weight = int(input())
+if watermelon_weight % 2 == 0 and watermelon_weight > 2:
+    print('YES')
+else:
+    print('NO')
