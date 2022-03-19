b477848b487c879c17d4af9a0b3ff1104c8db6e5
Ruslan_Zubtsov <Ruslan_Zubtsov@epam.com>
2022-03-20 00:38:33 +0400

    added way too long words problem from codeforces

---
+++ codeforces.com/71A/Solution.py
@@ -0,0 +1,8 @@
+number_of_words = int(input())
+
+for i in range(number_of_words):
+    word = input()
+    if len(word) > 10:
+        print(word[0] + str(len(word) - 2) + word[-1])
+    else:
+        print(word)
