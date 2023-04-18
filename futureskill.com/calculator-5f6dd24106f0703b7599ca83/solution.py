9b5d0b34bbc2ba4a4a49e5cdaa0d42d993ab5ae0
Ruslan Zubtsov <zubtsov.r@gmail.com>
2023-04-18 16:21:39 +0400

    https://futureskill.com/exercises/5f6dd24106f0703b7599ca83

---
+++ futureskill.com/calculator-5f6dd24106f0703b7599ca83/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def __init__(self, api):
+        self.api = api
+
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        return a / b
