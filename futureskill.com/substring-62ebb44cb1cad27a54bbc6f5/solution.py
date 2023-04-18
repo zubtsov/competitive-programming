0aee1faad9b77c48e041214797a9693cbbffbed1
Ruslan Zubtsov <zubtsov.r@gmail.com>
2023-04-18 16:52:59 +0400

    https://futureskill.com/exercises/62ebb44cb1cad27a54bbc6f5

---
+++ futureskill.com/substring-62ebb44cb1cad27a54bbc6f5/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def __init__(self, api):
+        self.api = api
+
+    def substring(self, input_string, start_index, end_index):
+        return input_string[start_index:end_index]
