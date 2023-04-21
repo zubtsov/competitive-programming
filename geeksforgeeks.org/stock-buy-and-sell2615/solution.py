64628e4406304a6d378225148641454b22239541
Ruslan Zubtsov <zubtsov.r@gmail.com>
2023-04-21 20:08:22 +0400

    https://practice.geeksforgeeks.org/problems/stock-buy-and-sell2615/0 refactored

--- geeksforgeeks.org/stock-buy-and-sell2615/solution.py
+++ geeksforgeeks.org/stock-buy-and-sell2615/solution.py
@@ -1,37 +1,25 @@
 def stockBuySell(price, n):
-    buy_sell_deals = []
-
+    is_profitable = False
     trading_day = 0
 
-    while trading_day < n - 1:
-        while not (
-                (trading_day == 0 and price[trading_day] < price[trading_day + 1]) or
-                (trading_day == n - 1 and price[trading_day - 1] > price[trading_day]) or
-                (price[trading_day - 1] > price[trading_day] and
-                 price[trading_day] < price[trading_day + 1])
-        ):
+    while trading_day < (n - 1):
+        while (trading_day < (n - 1)) and (price[trading_day] >= price[trading_day + 1]):
             trading_day += 1
-        last_local_minimum_day = trading_day
 
-        if trading_day >= n - 1:
+        if trading_day >= (n - 1):
             break
 
-        while not (
-                (trading_day == 0 and price[trading_day] > price[trading_day + 1]) or
-                (trading_day == n - 1 and price[trading_day - 1] < price[trading_day]) or
-                (price[trading_day - 1] < price[trading_day] and
-                 price[trading_day] > price[trading_day + 1])
-        ):
-            trading_day += 1
-        last_local_maximum_day = trading_day
-
-        delta = price[last_local_maximum_day] - price[last_local_minimum_day]
-        if delta > 0:
-            buy_sell_deals.append((last_local_minimum_day, last_local_maximum_day))
-
+        buy_day = trading_day
         trading_day += 1
 
-    if buy_sell_deals:
-        print(' '.join(map(lambda t: '({0} {1})'.format(t[0], t[1]), buy_sell_deals)))
-    else:
-        print('No Profit')
+        while (trading_day < n) and (price[trading_day] >= price[trading_day - 1]):
+            trading_day += 1
+
+        sell_day = trading_day - 1
+
+        print('({0} {1})'.format(buy_day, sell_day), end=' ')
+        is_profitable = True
+
+    if not is_profitable:
+        print('No Profit', end='')
+    print()
